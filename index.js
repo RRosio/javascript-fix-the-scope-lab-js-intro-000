2cfe97afcc56e88c89a467158ed43cac088b293c
Rosio G Reyes <rosio_reyes@yahoo.com>
2020-08-18 02:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function add2(n) {
   return n + two
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function () {
+  return function () {
     return "FUNKY!"
   }
 }
